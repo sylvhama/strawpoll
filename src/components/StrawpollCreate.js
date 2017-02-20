ee6dcd7d374f736f273c14eeb00d6b8d1f499fa8
Sylvain Hamann <sylvhama@gmail.com>
2017-02-20 17:30:38 +0900

    Added disable fcts

--- src/components/StrawpollCreate.js
+++ src/components/StrawpollCreate.js
@@ -15,35 +15,14 @@ class StrawpollCreate extends React.Component {
     this.addChoice = this.addChoice.bind(this);
     this.updateChoice = this.updateChoice.bind(this);
     this.removeChoice = this.removeChoice.bind(this);
+    this.isChoiceDeletable = this.isChoiceDeletable.bind(this);
+    this.isFormSubmitable = this.isFormSubmitable.bind(this);
   }
 
   state = {
+    wait: false,
     question: '',
     choices: ['', ''],
-  }
-
-  renderChoice(choice, i) {
-    const style = {
-      container: {position:'relative', paddingRight:'48px'},
-      icon: {position:'absolute', right:0},
-    }
-    return (
-      <div key={i}>
-        <div style={style.container}>
-          <TextField value={choice}
-                     onChange={(e, newValue) => this.updateChoice(newValue, i)}
-                     hintText={`Choice ${i+1}`}
-                     fullWidth={true}
-                     underlineShow={false}
-          />
-          <IconButton style={style.icon} 
-                      onClick={() => this.removeChoice(i)}>
-            <ContentClear />
-          </IconButton>
-        </div>
-        <Divider />
-      </div>
-    );
   }
 
   updateQuestion(newValue) {
@@ -70,8 +49,55 @@ class StrawpollCreate extends React.Component {
     this.setState({choices});
   }
 
-  render() {
+  isChoiceDeletable() {
+    const wait = this.state.wait;
+    if(wait) return false;
+    const choices = [...this.state.choices];
+    if(choices.length > 2) return true;
+    return false;
+  }
+
+  isFormSubmitable() {
+    const wait = this.state.wait;
+    if(wait) return false;
     const question = this.state.question,
+          choices = [...this.state.choices];
+    const index = choices.indexOf('');
+    if(index === -1 && question.length) return true;
+    return false;
+  }
+
+  renderChoice(choice, i) {
+    const wait = this.state.wait,
+          style = {
+            container: {position:'relative', paddingRight:'48px'},
+            icon: {position:'absolute', right:0},
+          };
+    return (
+      <div key={i}>
+        <div style={style.container}>
+          <TextField value={choice}
+                     onChange={(e, newValue) => this.updateChoice(newValue, i)}
+                     hintText={`Choice ${i+1}`}
+                     fullWidth={true}
+                     underlineShow={false}
+                     disabled={wait}
+          />
+          <IconButton style={style.icon} 
+                      onClick={() => this.removeChoice(i)}
+                      disabled={!this.isChoiceDeletable()}>
+            <ContentClear />
+          </IconButton>
+        </div>
+        <Divider />
+      </div>
+    );
+  }
+
+
+  render() {
+    const wait = this.state.wait,
+          question = this.state.question,
           choices = [...this.state.choices];
     return (
     	<Paper zDepth={1} className="paper">
@@ -81,6 +107,7 @@ class StrawpollCreate extends React.Component {
                      onChange={(e, newValue) => this.updateQuestion(newValue)}
                      hintText="Your question" 
                      fullWidth={true}
+                     disabled={wait}
           />
           <br />
           {choices.map((choice, i) => this.renderChoice(choice, i) )}
@@ -89,6 +116,14 @@ class StrawpollCreate extends React.Component {
             label="Add choice"
             icon={<ContentAdd />}
             onClick={this.addChoice}
+            disabled={wait}
+          />
+          <RaisedButton label="Publish Straw Poll" 
+                        type="submit"
+                        style={{marginTop: '2rem'}}
+                        primary={true}
+                        fullWidth={true}
+                        disabled={!this.isFormSubmitable()}
           />
         </form>
     	</Paper>
