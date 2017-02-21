eae8bc5813b3fdd854f4cfe23d570dbbbf5a3c03
Sylvain Hamann <sylvhama@gmail.com>
2017-02-21 19:05:00 +0900

    Prepared show view

--- src/components/StrawpollShow.js
+++ src/components/StrawpollShow.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import base from '../base';
+
 import Paper from 'material-ui/Paper';
 
-const StrawpollShow = () => (
-	<Paper zDepth={1} className="paper">
-	  <h2>StrawpollShow</h2>
-	</Paper>
-);
+class StrawpollShow extends React.Component {
+
+  state = {
+    question: '',
+    choices: [],
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    base.listenTo(`${id}`, {
+      context: this,
+      then(data) {
+        if(Object.keys(data).length) {
+          const question = data.question,
+                choices = [...data.choices];
+          this.setState({question, choices});
+        }else {
+          this.context.router.push('/');
+        }
+      }
+    });
+  }
+
+  render() {
+    const {question, choices} = this.state;
+    return (
+      <Paper zDepth={1} className="paper">
+        <h2>{`${question}`}</h2>
+      </Paper>
+    );
+  }
+}
+
+StrawpollShow.contextTypes = {
+  router: React.PropTypes.object
+};
 
 export default StrawpollShow;
